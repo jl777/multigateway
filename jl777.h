b79664bd00b204557511e01276576a851e0335e0
jl777 <jl777@users.noreply.github.com>
2014-03-11 04:02:42 +0200

    Update jl777.h

--- jl777.h
+++ jl777.h
@@ -1,3 +1,4 @@
+
 //  Created by jl777
 //  MIT License
 //
@@ -11,6 +12,13 @@
 #include <string.h>
 #include <memory.h>
 #include <math.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include "crypto_box.h"
+#include "randombytes.h"
+
+#define SESSION_CYCLE 10
 
 #ifndef COINCONFIG
 #define COINCONFIG "DOGE.h"
@@ -48,7 +56,6 @@
 
 #define SERVER_PORT 3005
 #define SERVER_PORTSTR "3005"
-#define GATEWAY_RETSIZE 0
 #define GATEWAY_SIG 0xdadafeed
 
 #define GENESISACCT "1739068987193023818"
@@ -64,13 +71,17 @@
 #define MAX_VOUTS 8
 #define DEPOSIT_FREQUENCY 3
 #define POLL_SECONDS 60
+#define MAX_ACTIVE_PEERS 20
 
 // defines
 // API funcids
+#define CREATE_NXTCOINS 'C'
 #define GET_COINDEPOSIT_ADDRESS 'g'
 #define SET_COINWITHDRAW_ADDRESS 'w'
+#define SEND_ANONYMOUS_PAYMENTS 'A'
 
 // gateway internal funcids
+#define START_NEW_SESSION 'N'
 #define BIND_DEPOSIT_ADDRESS 'b'
 #define PENDING_SWEEP 's'
 #define WITHDRAW_REQUEST '<'
@@ -92,7 +103,33 @@
 illegal GATEWAYID, must define GATEWAYID to 0, 1 or 2
 #endif
 
-struct strings { char **list; void **argptrs,**argptrs2; int64_t *args,*arg2; int num,max; };
+struct strings { char **list; void **argptrs,**argptrs2; int64_t *args,*arg2; int32_t num,max; };
+
+struct payment_bundle
+{
+    unsigned char escrow_pubkey[crypto_box_PUBLICKEYBYTES];
+    unsigned char depositaddr[MAX_NXTADDR_LEN];
+    unsigned char paymentacct_key[crypto_box_SECRETKEYBYTES];
+    unsigned char txouts[8][MAX_NXTADDR_LEN];
+    int64_t amounts[8],sessionid;
+};
+
+struct NXTcoins_data    // 1% of presale and mined goes to pooling acct, send (authorized - .99*presale) to pooling acct
+{
+    int64_t totalcoins __attribute__ ((packed));    // in satoshis
+    
+    int64_t presale __attribute__ ((packed));       // in satoshis, must be less than authorized and exactly match preissued
+    int64_t royalty __attribute__ ((packed));       // rate in satoshis, goes to issuer
+    int64_t bountyrate __attribute__ ((packed));    // rate in satoshis, goes to bountyfund
+    int64_t donation __attribute__ ((packed));      // rate in satoshis, goes to donationfund, defaulted to NXTcoins
+    int64_t blockrewards[16][2] __attribute__ ((packed));    // in satoshis
+
+    char coin_name[16];
+    char website[64],sourcecode[64];
+    char issuer[MAX_NXTADDR_LEN],poolingacct[MAX_NXTADDR_LEN],assetidstr[MAX_NXTADDR_LEN];
+    char bountyfund[MAX_NXTADDR_LEN],presalefund[MAX_NXTADDR_LEN],donationfund[MAX_NXTADDR_LEN];
+    
+};
 
 struct gateway_AM
 {
@@ -100,8 +137,8 @@ struct gateway_AM
     int32_t funcid __attribute__ ((packed));
     int32_t gatewayid __attribute__ ((packed));
     int32_t coinid __attribute__ ((packed));
-    int32_t timestamp __attribute__ ((packed));   // used as nonce
-    int32_t tbd2 __attribute__ ((packed));
+    int32_t timestamp __attribute__ ((packed));
+    int32_t sessionid __attribute__ ((packed));
     int32_t vout __attribute__ ((packed));
     int32_t tbd __attribute__ ((packed));
     
@@ -110,8 +147,22 @@ struct gateway_AM
     int64_t  change __attribute__ ((packed));
     int64_t  quantity __attribute__ ((packed));
     char NXTaddr[MAX_NXTADDR_LEN],coinaddr[MAX_COINADDR_LEN];
-    char txid[MAX_COINTXID_LEN],txid2[MAX_COINTXID_LEN],txid3[MAX_COINTXID_LEN],txid4[MAX_COINTXID_LEN];
-    char token[NXT_TOKEN_LEN];
+    union
+    {
+        struct NXTcoins_data coin;
+        struct
+        {
+            char txid[MAX_COINTXID_LEN],txid2[MAX_COINTXID_LEN],txid3[MAX_COINTXID_LEN],txid4[MAX_COINTXID_LEN];
+            char token[NXT_TOKEN_LEN];
+        };
+        struct
+        {
+            unsigned char publickey[crypto_box_PUBLICKEYBYTES];
+            unsigned char nonce[crypto_box_NONCEBYTES];
+            unsigned char paymentkey_by_prevrecv[crypto_box_PUBLICKEYBYTES + crypto_box_SECRETKEYBYTES + crypto_box_ZEROBYTES];
+            unsigned char payload_by_escrow[sizeof(struct payment_bundle) + crypto_box_ZEROBYTES];
+        };
+    };
 };
 
 struct gateway_state
@@ -125,16 +176,19 @@ struct active_NXTacct
     int32_t counter,numdeposits,maxdeposits,numsweeps,numtransfers,numredemptions,numwithdraws,numassets;
     int64_t total_deposits,total_transfers,total_assets_redeemed,total_withdraws;
     int64_t pending_sweepamount,pending_transfer,pending_redeem,pending_withdraw;
-    int64_t *balances;
-    char NXTaddr[MAX_NXTADDR_LEN],withdrawaddr[MAX_COINADDR_LEN],redeem_txid[MAX_COINTXID_LEN];
+    int32_t assetnonz,asseterrors,assetmissing;
+    int64_t *balances,nodeshares,nodecoins,current_nodecoins,nodecoins_sent;
+    char NXTaddr[MAX_NXTADDR_LEN],withdrawaddr[MAX_COINADDR_LEN],redeem_txid[MAX_COINTXID_LEN],ipaddr[INET6_ADDRSTRLEN];
     struct gateway_state gsm[NUM_GATEWAYS];
 };
+
+struct NXT_trade { int64_t price,quantity; char *buyer,*seller,*txid; int timestamp,assetid; };
 
 static char *Gateway_NXTaddrs[NUM_GATEWAYS] = { NXTACCTA, NXTACCTB, NXTACCTC };
 static char *Gateway_Pubkeys[NUM_GATEWAYS] = { PUBLICA, PUBLICB, PUBLICC };
 static char *Server_names[NUM_GATEWAYS] = { SERVER_NAMEA, SERVER_NAMEB, SERVER_NAMEC };
 static char NXTACCTSECRET[128]; // stored in plain text in RAM! suggest storing encrypted and decrypt only when needed
-static int Forged_minutes,RTflag;
+static int Forged_minutes,RTflag,BLOCK_ON_SERIOUS;
 
 static int is_gateway_AM(int *gatewayidp,struct gateway_AM *ap,char *sender,char *receiver)
 {
@@ -165,5 +219,7 @@ static int is_gateway_AM(int *gatewayidp,struct gateway_AM *ap,char *sender,char
 
 extern void update_gateway_states(int timestamp,struct gateway_AM *ap);
 extern void queue_asset_redemption(int timestamp,int gatewayid,struct active_NXTacct *active,char *recipient,char *redeem_txid,int64_t redeemtoshis,char *assetid);
+extern void update_asset_balances(char *assetname,char *change_txid,char *assetidstr,char *recipientstr,char *senderstr,int64_t satoshis);
+
 
 #endif

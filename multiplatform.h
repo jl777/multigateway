8ddeac34b9e293c1c7399cf09df11f37198098d8
cryptostalker <cryptostalker@gmail.com>
2014-03-23 05:38:37 -0400

    windows usleep proper microsecond resolution

--- multiplatform.h
+++ multiplatform.h
@@ -3,31 +3,39 @@
 
 int CloseSocket(int socket) {
 #ifndef _WIN32
-		// int close(int fd);
-		return close(socket);
+	// int close(int fd);
+	return close(socket);
 #else
-		// int closesocket(_In_  SOCKET s);
-		return closesocket(socket);
+	// int closesocket(_In_  SOCKET s);
+	return closesocket(socket);
 #endif
 } 
 
 void sleepOS(unsigned int seconds) {
 #ifndef _WIN32
-		// unsigned int sleep(unsigned int seconds)
-		sleep(seconds);
+	// unsigned int sleep(unsigned int seconds)
+	sleep(seconds);
 #else
-		// VOID WINAPI Sleep(DWORD dwMilliseconds)
-		Sleep(seconds * 1000);
+	// VOID WINAPI Sleep(DWORD dwMilliseconds)
+	Sleep(seconds * 1000);
 #endif
 } 
 
 void usleepOS(unsigned long usec) {
 #ifndef _WIN32
-		usleep(usec);
+	usleep(usec);
 #else
-		Sleep(usec / 1000);
+	// 
+http://stackoverflow.com/questions/5801813/c-usleep-is-obsolete-workarounds-for-windows-mingw
+    __int64 time1 = 0, time2 = 0, freq = 0;
+
+    QueryPerformanceCounter((LARGE_INTEGER *) &time1);
+    QueryPerformanceFrequency((LARGE_INTEGER *)&freq);
+
+    do {
+        QueryPerformanceCounter((LARGE_INTEGER *) &time2);
+    } while((time2-time1) < usec);
 #endif
 } 
 
 #endif
-
